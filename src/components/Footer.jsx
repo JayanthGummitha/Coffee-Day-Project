9e6d2c490563aed4bb887db4acbeb4c1effc513f
JayanthGummitha <jayanthgummitha@gmail.com>
2024-06-11 15:49:30 -0500

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { SiCoffeescript } from "react-icons/si";
+const Footer = () => {
+  return (
+    <div className=" bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E] text-black rounded-t-3xl mt-8 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
+        <div className=" w-full md:w-1/4">
+        <div className=" flex flex-row items-center cursor-pointer gap-2">
+            <span>
+              <SiCoffeescript size={25} />
+            </span>
+            <h1 className=" text-xl mt-4 font-semibold pb-4">CoffeeDay</h1>
+          </div>
+          <p className=" text-sm">
+            Welcome to our coffee haven! Explore our aromatic brews, savor
+            artisanal flavors, and discover the perfect roast to elevate your
+            daily ritual.
+          </p>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Links</h1>
+          <nav className=" flex flex-col gap-2">
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Menu
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              About Us
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Products
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Reviews
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Menu</h1>
+          <nav className=" flex flex-col gap-2">
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Cappuccino
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Latte
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Americano
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
+          <nav className=" flex flex-col gap-2">
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              jayanthgummitha@gmail.com
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              
+            </a>
+            <a
+              className=" hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Social media
+            </a>
+          </nav>
+        </div>
+      </div>
+      <div>
+        <p>
+          <p className=" text-center py-4">
+            @copyright developed by
+            <span className=" text-backgroundColor">
+              {" "}
+              Jayanth Gummitha{" "}
+            </span>
+            | All rights reserved
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
